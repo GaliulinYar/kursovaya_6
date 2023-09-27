d897f065946d73e127b5bbbd1449aba9a0daa7d7
Yaroslav Galiulin <yaroslav66@list.ru>
2023-09-27 11:19:08 +0500

    Урл логов рассылки

--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,14 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path, include
 from . import views
-from .views import MailingListView, mailinglogs, MailingCreateView, MailingDeleteView, \
-    MailingUpdateView, ClientListView, ClientDeleteView, ClientUpdateView, ClientCreateView
+from .views import MailingListView, MailingLog, MailingCreateView, MailingDeleteView, \
+    MailingUpdateView, ClientListView, ClientDeleteView, ClientUpdateView, ClientCreateView, MailingLogListView
 
 # from main.views import
 
 urlpatterns = [
     path('', MailingListView.as_view(), name='index'),
-    path('mailinglogs/', mailinglogs),
+    path('mailinglogs/', MailingLogListView.as_view(), name='логи'),
 
     path('create/', MailingCreateView.as_view(), name='create_mailing'),
     path('update/<int:pk>', MailingUpdateView.as_view(), name='update_mailing'),
